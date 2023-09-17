1bd40e2a66f739ea006e86b85897e0ce59ec49c1
dilan-schintha97 <dil97an@gmail.com>
2023-09-17 15:02:58 +0530

    done nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,48 +1,56 @@
-import './NavBar.css'
-import{AiOutlineMenu} from 'react-icons/ai'
+'use client'
+
+import { useState } from 'react';
+import './NavBar.css';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const NavBar = () => {
+
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
+
   return (
     <div className="n-wrapper">
       {/* desktop version */}
-        <div className="container">
-            <div className="n-container">
-               {/* left side */}
-               <div className="n-logo">
-                    <span>TEST APP</span>
-               </div>
+      <div className="container">
+        <div className="n-container">
+          {/* left side */}
+          <div className="n-logo">
+            <span>TEST APP</span>
+          </div>
 
-               {/* right-side */}
-               <div className="n-right">
-                    <div className="n-menu">
-                        <span>What we do</span>
-                        <span>How it works</span>
-                        <span>who we invest</span>
-                        <span>Testimonials</span>
-                    </div>
-                    <div className="found-button">
-                      Get funded
-                    </div>
-               </div>
+          {/* right-side */}
+          <div className="n-right">
+            <div className="n-menu">
+              <span>What we do</span>
+              <span>How it works</span>
+              <span>who we invest</span>
+              <span>Testimonials</span>
             </div>
-        </div>
-
-        {/* mobile/ tab version */}
-        <div className="nm-container">
-          <span>TEST APP</span>
-          <AiOutlineMenu  size={30}/>
-
-          {/* mobile menu */}
-          <div className="nm-menu">
-            <span>What we do</span>
-            <span>How it works</span>
-            <span>who we invest</span>
-            <span>Testimonials</span>
-            <div className="m-funded-button">Get funded</div>
+            <div className="found-button">
+              Get funded
+            </div>
           </div>
         </div>
+      </div>
+
+      {/* mobile/ tab version */}
+      <div className="nm-container">
+        <span>TEST APP</span>
+        {openMobileMenu ? (<AiOutlineClose onClick={(e) => { setOpenMobileMenu((prev) => !prev);}}  size={30}/>):( <AiOutlineMenu onClick={(e) => { setOpenMobileMenu((prev) => !prev);}}  size={30}/>)}
+       
+
+        {/* mobile menu */}
+        <div className="nm-menu" style={{ left: openMobileMenu ? "0px" : "-2000px" }}>
+          <span>What we do</span>
+          <span>How it works</span>
+          <span>who we invest</span>
+          <span>Testimonials</span>
+          <div className="m-funded-button">Get funded</div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default NavBar
+export default NavBar;
