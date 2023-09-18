24be16e7e9444c9246483bc311e73786f6e59c99
dilan-schintha97 <dil97an@gmail.com>
2023-09-18 19:22:19 +0530

    video section

---
+++ src/components/BrandingVideo/BrandingVideo.js
@@ -0,0 +1,15 @@
+import './BrandingVideo.css'
+
+const BrandingVideo = () => {
+  return (
+    <div className='bv-container'>
+      <div className="container">
+        <video className='bv-video'  loop muted autoPlay controls="">
+            <source src='./video.mp4' type='video/mp4' />
+        </video>
+      </div>
+    </div>
+  )
+}
+
+export default BrandingVideo
