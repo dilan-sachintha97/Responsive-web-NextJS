dc33f61e5889ea53e0aba7797eaccd3b829ee39e
dilan-schintha97 <dil97an@gmail.com>
2023-09-18 23:41:18 +0530

    what we do section 2

--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -26,15 +26,46 @@ const WhatWeDo = () => {
 
                         <div className="block-features">
                             {features?.slice(0,3).map((data,i)=>(
-                                <div key={i}>
+                                <div key={i} className='block-feature'>
                                     <Image src={data.icon} alt='feature' width={60} height={60}/>
+                                    <span>{data.title}</span>
                                 </div>
                             ))}
                         </div>
                     </div>
 
                     {/* second block */}
-                    <div className="wwd-block">second</div>
+                    <div className="wwd-block">
+                        <span className='sec-title'>Blue Seed</span>
+                        <span className='text'>Fund one-offs to scale e.g product, hiring</span>
+
+                        <div className="block-features">
+                            {features?.slice(3,).map((data,i)=>(
+                                <div key={i} className='block-feature'>
+                                    <Image src={data.icon} alt='feature' width={60} height={60}/>
+                                    <span>{data.title}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                {/* support blocks */}
+                <div className="wwd-support">
+
+                    {/* left side */}
+                    <div>
+                        <span className='sec-title'>Blue Growth Support</span>
+                        <span className='des'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non at aliquid molestiae nulla aliquam laboriosam?
+                        </span>
+                    </div>
+
+                    {/* right side */}
+                    <div>
+                        <span className='text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, veritatis!</span>
+
+                        <span className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi deserunt adipisci eaque harum distinctio. Odit, dicta in molestiae repellendus nulla vitae? Autem dignissimos corrupti, culpa quos expedita tempore consequuntur corporis?</span>
+                    </div>
                 </div>
             </div>
         </div>
