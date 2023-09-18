a0c56a7256814d3d3ba9202e70c91f888968a247
dilan-schintha97 <dil97an@gmail.com>
2023-09-18 21:57:14 +0530

    added animation to video

--- src/components/BrandingVideo/BrandingVideo.js
+++ src/components/BrandingVideo/BrandingVideo.js
@@ -1,12 +1,25 @@
+"use client"
+import { useRef } from 'react'
 import './BrandingVideo.css'
+import {motion, useScroll, useTransform} from 'framer-motion'
 
 const BrandingVideo = () => {
+
+    const ref = useRef(null);
+
+    const {scrollYProgress} = useScroll({
+        target:ref,
+        offset:["start end", "end end"]
+    })
+
+    const scale = useTransform(scrollYProgress, [0,1],[.6,1])
+
   return (
     <div className='bv-container'>
       <div className="container">
-        <video className='bv-video'  loop muted autoPlay controls="">
+        <motion.video style={{scale}}  ref={ref} className='bv-video'  loop muted autoPlay controls="">
             <source src='./video.mp4' type='video/mp4' />
-        </video>
+        </motion.video>
       </div>
     </div>
   )
