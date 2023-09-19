ae49e4fe1cfb70f6839307fbcf0e460f298a30f5
dilan-schintha97 <dil97an@gmail.com>
2023-09-19 12:47:13 +0530

    added animation

--- app/page.js
+++ app/page.js
@@ -1,17 +1,41 @@
+"use client"
 import NavBar from '@/src/components/NavBar/NavBar';
 import './page.css'
 import Hero from '@/src/components/Hero/Hero';
 import BrandingVideo from '@/src/components/BrandingVideo/BrandingVideo';
 import WhatWeDo from '@/src/components/WhatWeDo/WhatWeDo';
+import OurDiff from '@/src/components/OurDiff/OurDiff';
+import { motion, useAnimation } from 'framer-motion';
 
 export default function Home() {
 
+  const controls = useAnimation()
+
   return (
-    <div className='app'>
+    <motion.div className='app' animate={controls} >
         <NavBar/>
         <Hero/>
         <BrandingVideo/>
         <WhatWeDo/>
-    </div>
+        
+        <motion.div
+          onViewportEnter={()=>{
+            controls.start({
+              backgroundColor: "var(--secondary-color)",
+            })
+          }}
+
+          onViewportLeave={()=>{
+            controls.start({
+              backgroundColor:"white"
+            })
+          }}
+          
+          viewport={{amount:0.4}}
+        >
+         <OurDiff/>
+        </motion.div>
+      
+    </motion.div>
   );
 }
---
+++ src/components/OurDiff/OurDiff.js
@@ -0,0 +1,11 @@
+import './OurDiff.css'
+
+const OurDiff = () => {
+  return (
+    <div className='od-wrapper'>
+      ourdiff
+    </div>
+  )
+}
+
+export default OurDiff
