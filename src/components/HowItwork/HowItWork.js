7bfce8a91ddf687f6b9d4ba01ff2d1e6a768853b
dilan-schintha97 <dil97an@gmail.com>
2023-09-20 11:49:36 +0530

    set animation

--- src/components/HowItwork/HowItWork.js
+++ src/components/HowItwork/HowItWork.js
@@ -1,8 +1,27 @@
+"use client"
 import { hitFeatures } from '@/src/utils/data'
 import './HowItWork.css'
 import Image from 'next/image'
+import {motion} from 'framer-motion'
+import { tagVariants, titleVariants } from '@/src/utils/animation'
 
 const HowItWork = () => {
+
+    const featureVariants ={
+        offscreen:{
+            scale:0.5
+        },
+        onscreen:{
+            scale:1,
+            transition:{
+                type:"spring",
+                duration:1.5,
+
+            }
+        }
+    }
+
+
   return (
     <div className='hit-wrapper'>
       <div className="container">
@@ -10,8 +29,16 @@ const HowItWork = () => {
 
             {/* head */}
             <div className="hit-head">
-                <span className='tag'>How It works</span>
-                <span className='title'>Unlocking potential along the growth journey</span>
+                <motion.span
+                    variants={tagVariants}
+                    initial="offscreen"
+                    whileInView={"onscreen"} 
+                    className='tag'>How It works</motion.span>
+                <motion.span
+                    variants={titleVariants}
+                    initial="offscreen"
+                    whileInView={"onscreen"} 
+                    className='title'>Unlocking potential along the growth journey</motion.span>
             </div>
 
             {/* features */}
@@ -19,11 +46,22 @@ const HowItWork = () => {
                {hitFeatures?.map((data, index)=>(
                     <div key={index} className="hit-feature">
                         {/* left side */}
-                        <div className="detail">
+                        <motion.div 
+                        initial={{opacity:0, x:-100}}
+                        whileInView={{
+                            opacity:1,
+                            x:0,
+                            transition:{
+                                type:"easeIn",
+                                duration:1,
+                                delay:0.7
+                            }
+                        }}
+                        className="detail">
                             <span className="des">0{index+1}</span>
                             <span className="sec-title">{data.title}</span>
                             <span className="text">{data.des}</span>
-                        </div>
+                        </motion.div>
 
                         {/* right-side */}
                         <div className="icon">
